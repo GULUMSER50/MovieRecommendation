6df63123769e50c6fbb7f69347a37535d7237e13
GULUMSER50 <glmser_13@hotmail.com>
2024-08-18 22:23:36 +0300

    base model for MRS

---
+++ main.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve the API key from environment variables
+api_key = os.getenv('OPEN_AI_APIKEY')
+
+# Initialize the OpenAI client with the API key
+client = OpenAI(api_key=api_key)
+
+prompt = input("What kind of Movie are you thinking about?")
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+)
+
+print(completion.choices[0].message)
